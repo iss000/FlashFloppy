74814d0e2475097f7b9362e483e4331a05098c16
iss <iss@>
2019-05-28 23:39:02 +0300

    .

--- inc/intrinsics.h
+++ inc/intrinsics.h
@@ -13,6 +13,12 @@ struct exception_frame {
     uint32_t r0, r1, r2, r3, r12, lr, pc, psr;
 };
 
+#define _STR(x) #x
+#define STR(x) _STR(x)
+
+/* Force a compilation error if condition is true */
+#define BUILD_BUG_ON(cond) ({ _Static_assert(!(cond), "!(" #cond ")"); })
+
 #define __aligned(x) __attribute__((aligned(x)))
 #define __packed __attribute((packed))
 #define always_inline __inline__ __attribute__((always_inline))
